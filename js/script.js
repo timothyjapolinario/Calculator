cdb242116cbb5f6eccf75624bad06e2accae0e10
JayJay <timothyjapolinario@gmail.com>
2022-03-14 21:31:24 +0800

    Fix multiple operator inputs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 
 let numbersAndOperators = Array.from(document.querySelector(".numbers-operators").childNodes);
 let displayScreen = document.querySelector("#input-display");
-console.log(displayScreen);
+let backspace = document.querySelector("#backspace");
+let clear = document.querySelector("#clear");
+let equal = document.querySelector(".equal")
+let input = ""
+let isLastInputOperator = false;
+
+backspace.addEventListener('click', function(){
+    input = displayScreen.textContent.slice(0,-1);
+    displayScreen.textContent = input;
+})
+clear.addEventListener('click', function(){
+    input = ""
+    displayScreen.textContent = input;
+})
 
 numbersAndOperators.forEach(button=>{
     button.addEventListener('click',function(){
-        displayScreen.textContent += button.textContent
+        console.log(button.className);
+        console.log(isLastInputOperator);
+        if(!(isLastInputOperator &&(button.className=="operators"))){
+            input = button.textContent;
+            displayScreen.textContent += input;
+        }
+        isLastInputOperator = button.className == "operators"? true : false;    
     })
 })
 
