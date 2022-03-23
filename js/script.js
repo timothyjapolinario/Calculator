ccb711213b1d4d25ff06759f1903aaa65f2b56ee
JayJay <timothyjapolinario@gmail.com>
2022-03-23 11:07:57 +0800

    Add keyboard support

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ let currentComputedValue;
 let nextValue;
 let isEqualPressed = false
 backspace.addEventListener('click', function(){
+    backspacePress();    
+})
+clearButton.addEventListener('click', function(){
+    clearPress();
+})
+equalButton.addEventListener('click', function(){
+    equalPress();
+})
+numbers.forEach(button=>{
+    button.addEventListener('click', function(e){
+        numberPress(button.textContent);
+    })
+})
+operators.forEach(operator=>{
+    operator.addEventListener('click', function(){
+        operatorPress(operator.textContent);
+    })
+})
+
+function backspacePress(){
     textInput = displayScreen.textContent.slice(0,-1);
     if(historyScreen.textContent.slice(-1).match(/[\+ \- \× \÷]/g)){
         isLastInputOperator = false;
@@ -20,71 +40,12 @@ backspace.addEventListener('click', function(){
         displayScreen.textContent = textInput?textInput:0;
     }
     historyScreen.textContent = historyScreen.textContent.slice(0,-1);
-    
-})
-clearButton.addEventListener('click', function(){
+}
+
+function clearPress(){
     clearDisplay();
     clear();
-})
-equalButton.addEventListener('click', function(){
-    if(isLastInputOperator){}else{
-        operate();
-        isEqualPressed = true;
-        isLastInputOperator = false;
-    }
-})
-numbers.forEach(button=>{
-    button.addEventListener('click', function(e){
-        //if last input is not operator
-        if(isLastInputOperator){
-           clearDisplay(); 
-           isLastInputOperator = false;
-        }
-        if(button.textContent == "."){
-            if(!displayScreen.textContent.includes(".")){
-                displayScreen.textContent += button.textContent;
-                historyScreen.textContent += button.textContent;
-            }
-            return;
-        }
-        if(displayScreen.textContent.length < 20){
-            if(displayScreen.textContent == "0"){
-                clearDisplay();
-            }
-            displayScreen.textContent += button.textContent;
-            historyScreen.textContent += button.textContent;
-        }
-        button.prevent
-        isEqualPressed = false
-    })
-})
-operators.forEach(operator=>{
-    operator.addEventListener('click', function(){
-        //Replace the current selected operator
-        if(historyScreen.textContent.slice(-1).match(/[\+ \- \× \÷]/g)){
-            selectedOperator = operator;
-            historyScreen.textContent = historyScreen.textContent.slice(0,-1) + operator.textContent;
-            return;
-        }
-        //Chain operator
-        if((historyScreen.textContent.match(/[\+ \- \× \÷]/g) || []).length > 0){
-            operate();
-        }
-        selectedOperator = operator;
-        historyScreen.textContent += operator.textContent;
-        
-        //if there is no computedValue yet, get content
-        if(!currentComputedValue || nextValue == null){
-            currentComputedValue = parseFloat(displayScreen.textContent);
-        }else{
-            displayScreen.textContent = currentComputedValue;
-        }
-        isLastInputOperator = true;
-        isEqualPressed = false;
-    })
-})
-
-
+}
 
 function clearDisplay(){
     displayScreen.textContent = ""
@@ -103,7 +64,7 @@ function operate(){
     if(!isEqualPressed){
         nextValue = parseFloat(displayScreen.textContent);
     }
-    switch(selectedOperator.textContent){
+    switch(selectedOperator){
         case "+":
             currentComputedValue = add(currentComputedValue , nextValue);
             break;
@@ -121,9 +82,6 @@ function operate(){
     displayScreen.textContent = currentComputedValue;
     historyScreen.textContent = `${currentComputedValue}`
 }
-
-
-
 function add(a,b){
     console.log(`${a} + ${b}`)
     return (a+b);
@@ -140,4 +98,75 @@ function divide(a,b){
     console.log(`${a} / ${b}`)
     return (a/b);
 }
-
+function numberPress(inputNumber){
+        //if last input is not operator
+        if(isLastInputOperator){
+            clearDisplay(); 
+            isLastInputOperator = false;
+         }
+         if(inputNumber == "."){
+             if(!displayScreen.textContent.includes(".")){
+                 displayScreen.textContent += inputNumber;
+                 historyScreen.textContent += inputNumber;
+             }
+             return;
+         }
+         if(displayScreen.textContent.length < 20){
+             if(displayScreen.textContent == "0"){
+                 clearDisplay();
+             }
+             displayScreen.textContent += inputNumber;
+             historyScreen.textContent += inputNumber;
+         }
+         isEqualPressed = false
+}
+function operatorPress(operator){
+    //Replace the current selected operator
+    if(historyScreen.textContent.slice(-1).match(/[\+ \- \× \÷]/g)){
+        selectedOperator = operator;
+        historyScreen.textContent = historyScreen.textContent.slice(0,-1) + operator;
+        return;
+    }
+    //Chain operator
+    if((historyScreen.textContent.match(/[\+ \- \× \÷]/g) || []).length > 0){
+        operate();
+    }
+    selectedOperator = operator;
+    historyScreen.textContent += operator;
+    
+    //if there is no computedValue yet, get content
+    if(!currentComputedValue || nextValue == null){
+        currentComputedValue = parseFloat(displayScreen.textContent);
+    }else{
+        displayScreen.textContent = currentComputedValue;
+    }
+    isLastInputOperator = true;
+    isEqualPressed = false;
+}
+function equalPress(){
+    if(isLastInputOperator){}else{
+        operate();
+        isEqualPressed = true;
+        isLastInputOperator = false;
+    }
+}
+window.addEventListener("keydown",(e)=>{
+    if((e.key >= 0) && (e.key <= 9)){
+        numberPress(e.key);
+    }
+    if(e.key == "+"){
+        operatorPress(e.key)
+    }else if(e.key=="-"){
+        operatorPress("-")
+    }else if(e.key=="/"){
+        operatorPress("÷")
+    }else if(e.key=="*"){
+        operatorPress("×")
+    }else if(e.key == "Enter"){
+        equalPress()
+    }else if(e.key =="Backspace"){
+        backspacePress()
+    }else if(e.key == "Escape"){
+        clearPress();
+    }
+})
